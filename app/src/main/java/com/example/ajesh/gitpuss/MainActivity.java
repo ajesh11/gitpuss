b2bf10209a14b4f390bda1313bcc3eb08d24ec46
ajesh11 <ajeshlamichhane42@gmail.com>
2017-11-14 08:56:55 +0545

    adding file test.java

--- app/src/main/java/com/example/ajesh/gitpuss/MainActivity.java
+++ app/src/main/java/com/example/ajesh/gitpuss/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void testupload(){
         int i=1+1;
+        int ii=2+2;
     }
 }
---
+++ app/src/main/java/com/example/ajesh/gitpuss/testgit.java
@@ -0,0 +1,8 @@
+package com.example.ajesh.gitpuss;
+
+/**
+ * Created by ajesh on 2017-11-14.
+ */
+
+public class testgit {
+}
